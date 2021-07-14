eccafcce90c91c394c2a7ff3bf1eb36ac8f39e7a
fpatel3 <forampatel3010@gmail.com>
2021-07-14 09:22:09 -0400

    Made changes

--- WeatherLab/Program.cs
+++ WeatherLab/Program.cs
@@ -12,7 +12,8 @@ namespace WeatherLab
         {
             var measurements = new WeatherSqliteContext(dbfile).Weather;
 
-            var total_2020_precipitation = ?? TODO ??
+            var total_2020_precipitation = measurements.Where(measurement => measurement.year == 2020)
+                                            .Select(measurement => measurement.precipitation);
             Console.WriteLine($"Total precipitation in 2020: {total_2020_precipitation} mm\n");
 
             //
@@ -20,13 +21,31 @@ namespace WeatherLab
             //   see: https://en.wikipedia.org/wiki/Heating_degree_day
             //
 
-            // ?? TODO ??
+             var hddList = measurements
+                .Where(measurement => measurement.meantemp < 18)
+                .GroupBy(
+                    measurement => measurement.year,
+                    measurement => measurement.meantemp,
+                    (year, measurements) => new{
+                        year = year,
+                        count = measurements.Count()
+                    }
+                );
 
             //
             // Cooling degree days have a mean temp of >=18C
             //
 
-            // ?? TODO ??
+            var cddList = measurements
+                .Where(measurement => measurement.meantemp >= 18)
+                .GroupBy(
+                    measurement => measurement.year,
+                    measurement => measurement,
+                    (year, measurements) => new{
+                        year = year,
+                        count = measurements.Count()
+                    }
+                );
 
             //
             // Most Variable days are the days with the biggest temperature
@@ -42,12 +61,41 @@ namespace WeatherLab
             //
             Console.WriteLine("Year\tHDD\tCDD");
 
-            // ?? TODO ??
+            var yearlyMeasurements = cddList
+                .Join(
+                    hddList,
+                    cdd => cdd.year,
+                    hdd => hdd.year,
+                    (cdd, hdd) => new
+                    {
+                        year = cdd.year,
+                        hdd = hdd.count,
+                        cdd = cdd.count
+                    }
+                ).OrderBy(yearlyMeasurement=> yearlyMeasurement.year);
+
+            foreach (var yearlyMeasurement in yearlyMeasurements)
+            {
+                Console.WriteLine($"{yearlyMeasurement.year:d4}\t{yearlyMeasurement.hdd:d4}\t{yearlyMeasurement.cdd:d4}");
+            }
 
             Console.WriteLine("\nTop 5 Most Variable Days");
             Console.WriteLine("YYYY-MM-DD\tDelta");
 
-            // ?? TODO ??
+            var dayTemperatureVariationMeasuremetns = measurements.Select(measurement => new
+                                                        {
+                                                            year = measurement.year,
+                                                            month = measurement.month,
+                                                            day = measurement.day,
+                                                            delta = measurement.maxtemp - measurement.mintemp,
+                                                        });
+                var top5TemperatureVariationDays = dayTemperatureVariationMeasuremetns
+                                                    .OrderByDescending(measurement=>measurement.delta)
+                                                    .Take(5);
+             foreach(var dayMeasurement in top5TemperatureVariationDays)
+            {
+                Console.WriteLine($"{dayMeasurement.year:d4}-{dayMeasurement.month:d2}-{dayMeasurement.day:d2}\t{dayMeasurement.delta:0.00}");
+            }    
         }
     }
 }
